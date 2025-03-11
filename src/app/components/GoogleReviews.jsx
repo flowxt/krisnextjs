9473b05df92ed6ac9a75d5f825915837d8713a42
florian <florianbarjon@icloud.com>
2025-03-11 17:01:07 +0100

    google

--- src/app/components/GoogleReviews.jsx
+++ src/app/components/GoogleReviews.jsx
@@ -121,27 +121,27 @@ export default function GoogleReviews() {
         </div>
         
         {/* Boutons navigation */}
-        <div className="absolute top-1/2 -translate-y-1/2 w-full flex justify-between px-2">
-          <button 
-            onClick={handlePrevReview}
-            className="w-8 h-8 rounded-full bg-white shadow-md flex items-center justify-center text-purple-600 hover:bg-purple-50"
-            aria-label="Avis précédent"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
-            </svg>
-          </button>
-          
-          <button 
-            onClick={handleNextReview}
-            className="w-8 h-8 rounded-full bg-white shadow-md flex items-center justify-center text-purple-600 hover:bg-purple-50"
-            aria-label="Avis suivant"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div>
+        <div className="absolute top-1/2 -translate-y-1/2 w-full hidden md:flex justify-between px-2">
+  <button 
+    onClick={handlePrevReview}
+    className="w-8 h-8 rounded-full bg-white shadow-md flex items-center justify-center text-purple-600 hover:bg-purple-50"
+    aria-label="Avis précédent"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+    </svg>
+  </button>
+  
+  <button 
+    onClick={handleNextReview}
+    className="w-8 h-8 rounded-full bg-white shadow-md flex items-center justify-center text-purple-600 hover:bg-purple-50"
+    aria-label="Avis suivant"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+    </svg>
+  </button>
+</div>
         
         {/* Indicateurs de pagination */}
         <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 flex space-x-2">
